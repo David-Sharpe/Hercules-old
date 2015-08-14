0517920cde7681ecec78328d43d83ece6f5af1ef
David Sharpe <davidusbviolator@gmail.com>
2015-08-14 06:49:54 -0400

    User has workouts

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+  has_many :resistance_workouts
+  has_many :resistance_exercises, through: :resistance_workouts
 end
--- db/migrate/20150801012522_create_resistance_workouts.rb
+++ db/migrate/20150801012522_create_resistance_workouts.rb
@@ -2,8 +2,6 @@ class CreateResistanceWorkouts < ActiveRecord::Migration
   def change
     create_table :resistance_workouts do |t|
       t.references :user, index: true
-      t.references :exercise, index: true
-
       t.timestamps
     end
   end
