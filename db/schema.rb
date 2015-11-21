125e9197eea209030a045895a6000ec1660e431b
David Sharpe <davidusbviolator@gmail.com>
2015-11-21 14:10:46 -0500

    Load workout_template into db

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150801012845) do
+ActiveRecord::Schema.define(version: 20151121190931) do
 
-  create_table "resistance_exercises", force: true do |t|
-    t.string   "name"
+  create_table "resistance_exercises", force: :cascade do |t|
+    t.string   "name",        limit: 255
     t.decimal  "resistance"
-    t.string   "unit"
+    t.string   "unit",        limit: 255
     t.integer  "repetitions"
     t.integer  "sets"
     t.datetime "created_at"
@@ -26,32 +26,41 @@ ActiveRecord::Schema.define(version: 20150801012845) do
 
   add_index "resistance_exercises", ["workout_id"], name: "index_resistance_exercises_on_workout_id"
 
-  create_table "resistance_workouts", force: true do |t|
+  create_table "resistance_workouts", force: :cascade do |t|
     t.integer  "user_id"
-    t.integer  "exercise_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "resistance_workouts", ["exercise_id"], name: "index_resistance_workouts_on_exercise_id"
   add_index "resistance_workouts", ["user_id"], name: "index_resistance_workouts_on_user_id"
 
-  create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                      default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "workout_templates", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "user_id_id"
+    t.integer  "period_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "workout_templates", ["period_id"], name: "index_workout_templates_on_period_id"
+  add_index "workout_templates", ["user_id_id"], name: "index_workout_templates_on_user_id_id"
 
 end
